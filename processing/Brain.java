ad1b2cb531e41a9e3b474429159ce9b76c050c25
tiut.nicolae <tiut.nicolae@86c32ec8-2222-c1ef-e6d3-73eb86dbe42b>
2010-05-28 14:38:29 +0000

    ChangeTest 1

--- processing/Brain.java
+++ processing/Brain.java
@@ -33,6 +33,8 @@ public class Brain implements Runnable {
      * If yes, we will process and compare that soon.
      */
     private boolean capturedImageChanged;
+
+    private boolean algorithmRunning;
     
     private int result;
     
@@ -41,6 +43,7 @@ public class Brain implements Runnable {
         this.parentModel = m;
         
         this.capturedImageChanged = false;
+        this.algorithmRunning = false;
         this.result = -1;        
     }
 
@@ -71,7 +74,7 @@ public class Brain implements Runnable {
         int currentResult = this.result;
 
         while (true) {
-            
+
             /*
              * This is the normal sleep that relaxes the CPU a bit and
              * lets other threads run fluently.
@@ -82,128 +85,131 @@ public class Brain implements Runnable {
                 ex.printStackTrace();
             }
 
-            if (this.capturedImageChanged) {
-                synchronized (this.lockObject) {
-                    this.capturedImageChanged = false;
-                }
+            while (this.algorithmRunning) {
 
-                /*
-                 * This is where the Algorithm method will be called (possibly other classes involded).
-                 * For now, instead a simple sleep will be called as simulation.
-                 *
-                 *   try {
-                 *       Thread.sleep(5000);
-                 *   } catch (InterruptedException ex) {
-                 *       ex.printStackTrace();
-                 *   }
-                 */
-                
-                /*
-                 * Let's see.
-                 * Processing and information aquiring part.
-                 */
-                if (this.siblingEye == null) {
-                    continue;
-                }
-                BufferedImage capturedIm = this.siblingEye.getImage();
-                if (capturedIm == null) {
-                    continue;
-                }                
-                double ar = (double) EyeWebcam.HAND_CUT_X2 - EyeWebcam.HAND_CUT_X1 / EyeWebcam.HAND_CUT_Y2 - EyeWebcam.HAND_CUT_Y1;
-                int camImWidth, camImHeight;
-                if (ar < 1.333) {
-                    camImWidth = (int) Math.round(DBImage.DB_IMAGE_HEIGHT * ar);
-                    camImHeight = DBImage.DB_IMAGE_HEIGHT;
-                } else {
-                    camImWidth = DBImage.DB_IMAGE_WIDTH;
-                    camImHeight = (int) Math.round(DBImage.DB_IMAGE_WIDTH / ar);
-                }
-                int[][] cutResizedGrayIntIm = ImageAlgorithms.buffIm2CutGrayResizedIntIm(capturedIm, EyeWebcam.HAND_CUT_X1, EyeWebcam.HAND_CUT_Y1, EyeWebcam.HAND_CUT_X2, EyeWebcam.HAND_CUT_Y2, camImWidth, camImHeight);
-                GrayImageAndHistogram contourIntImAndHistogram = ImageAlgorithms.grayIntIm2ContourImAndHistogram(cutResizedGrayIntIm, DBImage.CONTOUR_POWER);
-                int[][] contourIntIm = contourIntImAndHistogram.getGrayImage();
-                int[] histogram = contourIntImAndHistogram.getHistogram();
-                int treshold = ImageAlgorithms.computeNecessaryThreshold(DBImage.WHITE_PROPORTION, histogram, camImWidth * camImHeight);
-                boolean[][] camBoolIm = ImageAlgorithms.grayIntIm2BoolIm(contourIntIm, treshold);
-                Shape greatestShape = ImageAlgorithms.findGreatestShape(camBoolIm);
+                if (this.capturedImageChanged) {
+                    synchronized (this.lockObject) {
+                        this.capturedImageChanged = false;
+                    }
 
-                Point2D leftMostPoint = greatestShape.getLeftMostPoint();
-                Point2D rightMostPoint = greatestShape.getRightMostPoint();
-                Point2D bottomMostPoint = greatestShape.getBottomMostPoint();
-                Point2D topMostPoint = greatestShape.getTopMostPoint();
+                    /*
+                     * This is where the Algorithm method will be called (possibly other classes involded).
+                     * For now, instead a simple sleep will be called as simulation.
+                     *
+                     *   try {
+                     *       Thread.sleep(5000);
+                     *   } catch (InterruptedException ex) {
+                     *       ex.printStackTrace();
+                     *   }
+                     */
 
-                Shape leftShape = ImageAlgorithms.reduceShapeHVAreaLimit(greatestShape, leftMostPoint, DBImage.HV_THINNESS, true, DBImage.HV_AREA_LIMIT);
-                Shape rightShape = ImageAlgorithms.reduceShapeHVAreaLimit(greatestShape, rightMostPoint, DBImage.HV_THINNESS, true, DBImage.HV_AREA_LIMIT);
-                Shape topShape = ImageAlgorithms.reduceShapeHVAreaLimit(greatestShape, topMostPoint, DBImage.HV_THINNESS, false, DBImage.HV_AREA_LIMIT);
-                Shape bottomShape = ImageAlgorithms.reduceShapeHVAreaLimit(greatestShape, bottomMostPoint, DBImage.HV_THINNESS, false, DBImage.HV_AREA_LIMIT);
-
-                int camLeftShapeCenter = leftShape.getCenter().getX();
-                int camTopShapeCenter = topShape.getCenter().getY();
-                int camShapeWidth = rightShape.getCenter().getX() - camLeftShapeCenter;
-                int camShapeHeight = bottomShape.getCenter().getY() - camTopShapeCenter;
-
-                /* Comparison part.
-                 * Similar to that sucky algorithm Iulia showed me.
-                 * (The algorithm is sucky not Iulia)
-                 * BTW talkin'bout Iulia M., not Iulia P. Cause Iulia P. _is_ forsure.
-                 */
-                Vector<Letter> letters = this.parentModel.getLetters();                
-                if (letters == null) {
-                    continue;
-                }
-                Iterator<Letter> itlt = letters.iterator();
-                int letterIndex = 0;
-                int maxMatched = 0;
-                int maxMatchedIndex = 0;
-                while (itlt.hasNext()) {
-                    Letter letter = itlt.next();
-                    Vector<DBImage> dbIms = letter.getDBImages();
-                    if (dbIms == null) {
+                    /*
+                     * Let's see.
+                     * Processing and information aquiring part.
+                     */
+                    if (this.siblingEye == null) {
                         continue;
                     }
-                    Iterator<DBImage> itim = dbIms.iterator();
-                    int imIndex = 0;
-                    int nMatched = 0;
-                    while (itim.hasNext()) {
-                        DBImage dbIm = itim.next();
+                    BufferedImage capturedIm = this.siblingEye.getImage();
+                    if (capturedIm == null) {
+                        continue;
+                    }
+                    double ar = (double) EyeWebcam.HAND_CUT_X2 - EyeWebcam.HAND_CUT_X1 / EyeWebcam.HAND_CUT_Y2 - EyeWebcam.HAND_CUT_Y1;
+                    int camImWidth, camImHeight;
+                    if (ar < 1.333) {
+                        camImWidth = (int) Math.round(DBImage.DB_IMAGE_HEIGHT * ar);
+                        camImHeight = DBImage.DB_IMAGE_HEIGHT;
+                    } else {
+                        camImWidth = DBImage.DB_IMAGE_WIDTH;
+                        camImHeight = (int) Math.round(DBImage.DB_IMAGE_WIDTH / ar);
+                    }
+                    int[][] cutResizedGrayIntIm = ImageAlgorithms.buffIm2CutGrayResizedIntIm(capturedIm, EyeWebcam.HAND_CUT_X1, EyeWebcam.HAND_CUT_Y1, EyeWebcam.HAND_CUT_X2, EyeWebcam.HAND_CUT_Y2, camImWidth, camImHeight);
+                    GrayImageAndHistogram contourIntImAndHistogram = ImageAlgorithms.grayIntIm2ContourImAndHistogram(cutResizedGrayIntIm, DBImage.CONTOUR_POWER);
+                    int[][] contourIntIm = contourIntImAndHistogram.getGrayImage();
+                    int[] histogram = contourIntImAndHistogram.getHistogram();
+                    int treshold = ImageAlgorithms.computeNecessaryThreshold(DBImage.WHITE_PROPORTION, histogram, camImWidth * camImHeight);
+                    boolean[][] camBoolIm = ImageAlgorithms.grayIntIm2BoolIm(contourIntIm, treshold);
+                    Shape greatestShape = ImageAlgorithms.findGreatestShape(camBoolIm);
 
-                        boolean[][] dbBoolIm = dbIm.getRaster();
-                        int dbLeftShapeCenter = dbIm.getLeftShapeCenter();
-                        int dbTopShapeCenter = dbIm.getTopShapeCenter();
-                        int dbShapeWidth = dbIm.getShapeWidth();
-                        int dbShapeHeight = dbIm.getShapeHeight();
-                        boolean[][] transZoomedBoolIm = ImageAlgorithms.transZoomBoolIm(camBoolIm, dbLeftShapeCenter - camLeftShapeCenter, dbTopShapeCenter - camTopShapeCenter, (double) dbShapeWidth / camShapeWidth, (double) dbShapeHeight / camShapeHeight, camLeftShapeCenter, camTopShapeCenter);
+                    Point2D leftMostPoint = greatestShape.getLeftMostPoint();
+                    Point2D rightMostPoint = greatestShape.getRightMostPoint();
+                    Point2D bottomMostPoint = greatestShape.getBottomMostPoint();
+                    Point2D topMostPoint = greatestShape.getTopMostPoint();
 
-                        double match = ImageAlgorithms.compareTwoBoolIms(transZoomedBoolIm, dbBoolIm);
-                        if (match >= Brain.MINIMUM_RATE_NEEDED_TO_MATCH) {
-                            nMatched++;
+                    Shape leftShape = ImageAlgorithms.reduceShapeHVAreaLimit(greatestShape, leftMostPoint, DBImage.HV_THINNESS, true, DBImage.HV_AREA_LIMIT);
+                    Shape rightShape = ImageAlgorithms.reduceShapeHVAreaLimit(greatestShape, rightMostPoint, DBImage.HV_THINNESS, true, DBImage.HV_AREA_LIMIT);
+                    Shape topShape = ImageAlgorithms.reduceShapeHVAreaLimit(greatestShape, topMostPoint, DBImage.HV_THINNESS, false, DBImage.HV_AREA_LIMIT);
+                    Shape bottomShape = ImageAlgorithms.reduceShapeHVAreaLimit(greatestShape, bottomMostPoint, DBImage.HV_THINNESS, false, DBImage.HV_AREA_LIMIT);
+
+                    int camLeftShapeCenter = leftShape.getCenter().getX();
+                    int camTopShapeCenter = topShape.getCenter().getY();
+                    int camShapeWidth = rightShape.getCenter().getX() - camLeftShapeCenter;
+                    int camShapeHeight = bottomShape.getCenter().getY() - camTopShapeCenter;
+
+                    /* Comparison part.
+                     * Similar to that sucky algorithm Iulia showed me.
+                     * (The algorithm is sucky not Iulia)
+                     * BTW talkin'bout Iulia M., not Iulia P. Cause Iulia P. _is_ forsure.
+                     */
+                    Vector<Letter> letters = this.parentModel.getLetters();
+                    if (letters == null) {
+                        continue;
+                    }
+                    Iterator<Letter> itlt = letters.iterator();
+                    int letterIndex = 0;
+                    int maxMatched = 0;
+                    int maxMatchedIndex = 0;
+                    while (itlt.hasNext()) {
+                        Letter letter = itlt.next();
+                        Vector<DBImage> dbIms = letter.getDBImages();
+                        if (dbIms == null) {
+                            continue;
+                        }
+                        Iterator<DBImage> itim = dbIms.iterator();
+                        int imIndex = 0;
+                        int nMatched = 0;
+                        while (itim.hasNext()) {
+                            DBImage dbIm = itim.next();
+
+                            boolean[][] dbBoolIm = dbIm.getRaster();
+                            int dbLeftShapeCenter = dbIm.getLeftShapeCenter();
+                            int dbTopShapeCenter = dbIm.getTopShapeCenter();
+                            int dbShapeWidth = dbIm.getShapeWidth();
+                            int dbShapeHeight = dbIm.getShapeHeight();
+                            boolean[][] transZoomedBoolIm = ImageAlgorithms.transZoomBoolIm(camBoolIm, dbLeftShapeCenter - camLeftShapeCenter, dbTopShapeCenter - camTopShapeCenter, (double) dbShapeWidth / camShapeWidth, (double) dbShapeHeight / camShapeHeight, camLeftShapeCenter, camTopShapeCenter);
+
+                            double match = ImageAlgorithms.compareTwoBoolIms(transZoomedBoolIm, dbBoolIm);
+                            if (match >= Brain.MINIMUM_RATE_NEEDED_TO_MATCH) {
+                                nMatched++;
+                            }
+
+                            imIndex++;
+                        }
+                        if (nMatched > maxMatched) {
+                            maxMatched = nMatched;
+                            maxMatchedIndex = letterIndex;
                         }
 
-                        imIndex++;
-                    }
-                    if (nMatched > maxMatched) {
-                        maxMatched = nMatched;
-                        maxMatchedIndex = letterIndex;
+                        letterIndex++;
                     }
 
-                    letterIndex++;
+                    /* Evaluating the comparison.
+                     */
+                    if (maxMatched > Brain.MINIMUM_NUMBER_NEEDED_TO_MATCH) {
+                        currentResult = maxMatchedIndex;
+                    } else {
+                        currentResult = -1;
+                    }
+
+                    /* For testing:
+                     * currentResult = r.nextInt(100);
+                     */
                 }
 
-                /* Evaluating the comparison.
-                 */
-                if (maxMatched > Brain.MINIMUM_NUMBER_NEEDED_TO_MATCH) {
-                    currentResult = maxMatchedIndex;
-                } else {
-                    currentResult = -1;
-                }                
-
-                /* For testing:
-                 * currentResult = r.nextInt(100);
-                 */
-            }
-            
-            if (currentResult != this.result) {                
-                this.result = currentResult;
-                this.parentModel.setBrainResultChanged();
+                if (currentResult != this.result) {
+                    this.result = currentResult;
+                    this.parentModel.setBrainResultChanged();
+                }
             }
         }
     }    
